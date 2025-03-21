bff874d2737d49b7f6c6550af72c85c96d667599
edgaro9 <edgaro@cpp.edu>
2025-03-20 23:44:38 -0700

    Enabled user signup

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -6,6 +6,8 @@ function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [full_name,setName] = useState("");
+  const [avatar_url,setAvatarUrl] = useState("");
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -14,6 +16,13 @@ function Register() {
     const { data, error } = await supabase.auth.signUp({
       email: email,
       password: password,
+      options:{
+        data:{
+          full_name :full_name,
+          avatar_url: avatar_url
+        },
+      },
+    
     });
 
     if (error) {
@@ -27,15 +36,18 @@ function Register() {
 
     setEmail("");
     setPassword("");
+    setName("");
+    setAvatarUrl("");
   };
 
   return (
-    <div>
+    <div className = "bg-gray-200 ">
       <h2>Register</h2>
       <br></br>
       {message && <span>{message}</span>}
       <form onSubmit={handleSubmit}>
-        <input
+        <input 
+          className="border border-black text-black"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
           type="email"
@@ -43,12 +55,31 @@ function Register() {
           required
         />
         <input
+          className="border border-black text-black"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
           type="password"
           placeholder="Password"
           required
         />
+        <input 
+            className="border border-black text-black"
+            onChange={(e) => setName(e.target.value)}
+            value = {full_name}
+            type = "Name"
+            placeholder="Name"
+            required
+        />
+
+        <input 
+            className="border border-black text-black"
+            onChange={(e) => setAvatarUrl(e.target.value)}
+            value = {avatar_url}
+            type = "text"
+            placeholder="Avatar URL"
+            required
+        />
+        
         <button type="submit">Create Account</button>
       </form>
       <span>Already have an account?</span>
